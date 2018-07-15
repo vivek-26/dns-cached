77a561654bf597645c295d2900e0a284079c553b
Vivek Kumar <vivek.kumar26@live.com>
2018-07-15 18:35:20 +0530

    chore(dummy): add dummy code

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/**
+ * @author Vivek Kumar <vivek.kumar26@live.com>
+ * @overview Index file of the module.
+ * @copyright Vivek Kumar 2018
+ * @license MIT
+ */
+const exampleFn = () => 'DNS Cache for Node.js';
+
+module.exports = { exampleFn };
