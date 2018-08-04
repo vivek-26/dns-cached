16dde126cbdc5705d7200070b1ee0ebed8f5e289
Vivek Kumar <vivek.kumar26@live.com>
2018-08-04 19:47:06 +0530

    feat(override methods): added options for specifying the dns methods to be memoized

--- src/cached-dns/index.js
+++ src/cached-dns/index.js
@@ -11,7 +11,7 @@ import { getAllDnsMethods, memoizeDnsMethods } from './memoize-dns';
 /* eslint-disable arrow-parens */
 
 /** Cached DNS Class - Override DNS Methods */
-class CachedDNS {
+export class CachedDNS {
   /**
    * Creates an instance of CachedDNS.
    * @param {number} [ttl] TTL value for cache items in minutes.
@@ -27,6 +27,8 @@ class CachedDNS {
 
     CachedDNS.$instance = this;
 
+    this.methods = null;
+
     /* Create Cache Store Object (Static) */
     CachedDNS.cacheObj = createCacheStore(ttl, config);
   }
@@ -36,8 +38,28 @@ class CachedDNS {
    * @returns {boolean}
    * @memberof CachedDNS
    */
-  overrideMethods() {
-    memoizeDnsMethods(getAllDnsMethods(), CachedDNS.cacheObj);
+  overrideMethods(options) {
+    const allDnsMethods = getAllDnsMethods();
+
+    if (!options) {
+      this.methods = allDnsMethods;
+    } else if (typeof options === 'string') {
+      if (allDnsMethods.includes(options)) {
+        this.methods = [options];
+      } else {
+        throw new Error(`Invalid DNS Method - '${options}'`);
+      }
+    } else if (Array.isArray(options)) {
+      if (options.every((m) => allDnsMethods.includes(m))) {
+        this.methods = options;
+      } else {
+        throw new Error('An invalid dns method was passed');
+      }
+    } else {
+      throw new TypeError('Expected a string or an array of strings');
+    }
+
+    memoizeDnsMethods(this.methods, CachedDNS.cacheObj);
     return true;
   }
 }
