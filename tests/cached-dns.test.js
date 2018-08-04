d1d14bfc95716548ad663b53f386a9360c7fd235
Vivek Kumar <vivek.kumar26@live.com>
2018-08-04 19:47:59 +0530

    test(override methods): add tests for overriding methods options

--- tests/cached-dns.test.js
+++ tests/cached-dns.test.js
@@ -5,7 +5,7 @@
  * @license MIT
  */
 import * as memoizeDns from '../src/cached-dns/memoize-dns';
-import dnsCached from '../src/cached-dns';
+import dnsCached, { CachedDNS } from '../src/cached-dns';
 
 describe('CachedDNS Module Tests', () => {
   const cachedDns = dnsCached(0.00083);
@@ -17,6 +17,58 @@ describe('CachedDNS Module Tests', () => {
   /* An instance is already created above */
   test('Multiple instances of CachedDNS cannot be created', () => {
     expect(dnsCached).toThrow(/already has an instance!/);
+  });
+
+  test('overrideMethods memoizes all methods when options is empty', () => {
+    CachedDNS.$instance = undefined;
+    const cacheddns = new CachedDNS(0.00083);
+    cacheddns.overrideMethods();
+    expect(Array.isArray(cacheddns.methods)).toBeTruthy();
+    expect(cacheddns.methods.length).toBe(memoizeDns.getAllDnsMethods().length);
+  });
+
+  test('overrideMethods memoizes a single method passed as string', () => {
+    CachedDNS.$instance = undefined;
+    const cacheddns = new CachedDNS(0.00083);
+    cacheddns.overrideMethods('lookup');
+    expect(Array.isArray(cacheddns.methods)).toBeTruthy();
+    expect(cacheddns.methods.length).toBe(1);
+  });
+
+  test('overrideMethods memoizes throws an error when invalid function name is passed', () => {
+    CachedDNS.$instance = undefined;
+    const cacheddns = new CachedDNS(0.00083);
+    expect(cacheddns.overrideMethods.bind(null, 'shouldthrowerror')).toThrow(
+      /Invalid DNS Method/,
+    );
+  });
+
+  test('overrideMethods memoizes multiple methods passes as an array', () => {
+    CachedDNS.$instance = undefined;
+    const cacheddns = new CachedDNS(0.00083);
+    cacheddns.overrideMethods(['lookup', 'resolve', 'lookupService']);
+    expect(Array.isArray(cacheddns.methods)).toBeTruthy();
+    expect(cacheddns.methods.length).toBe(3);
+  });
+
+  test('overrideMethods memoizes throws error when an invalid method is present in array', () => {
+    CachedDNS.$instance = undefined;
+    const cacheddns = new CachedDNS(0.00083);
+    expect(
+      cacheddns.overrideMethods.bind(null, [
+        'lookup',
+        'resolve',
+        'lookupServices',
+      ]),
+    ).toThrow(/An invalid dns method was passed/);
+  });
+
+  test('overrideMethods memoizes throws error when invalid type is passed as argument', () => {
+    CachedDNS.$instance = undefined;
+    const cacheddns = new CachedDNS(0.00083);
+    expect(cacheddns.overrideMethods.bind(null, 12345)).toThrow(
+      /Expected a string or an array of strings/,
+    );
   });
 
   test('overrideMethods calls all necessary methods', () => {
